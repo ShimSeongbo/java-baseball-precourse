b7c65a44fe120c906d3d718bd84eed99e9395bbc
shimseongbo <seong265@icloud.com>
2023-09-11 13:35:58 +0900

    클래스 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -27,38 +27,6 @@ public class Application {
     }
 }
 
-class GameRunner {
-    private final Scanner scanner;
-
-    public GameRunner(Scanner scanner) {
-        this.scanner = scanner;
-    }
-
-    public void runGame() {
-        NumberGenerator generator = new NumberGenerator();
-        List<Integer> computerNumbers = generator.generateNumbers();
-        GuessChecker guessChecker = new GuessChecker(computerNumbers);
-
-        while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            int guess = scanner.nextInt();
-            if (!InputValidator.isValid(guess)) {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
-            }
-
-            List<Integer> userGuess = InputParser.parse(guess);
-            GuessResult result = guessChecker.checkGuess(userGuess);
-
-            if (result.isGameWon()) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                return;
-            }
-
-            result.showHint();
-        }
-    }
-}
-
 class NumberGenerator {
     public List<Integer> generateNumbers() {
         List<Integer> numbers = new ArrayList<>();
